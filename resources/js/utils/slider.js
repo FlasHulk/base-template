c64f437d9972225335c2775d08e7e4180e0ce87f
AndreyShramov <andrey.rocketcompany@gmail.com>
2023-06-10 18:11:13 +0300

    new template

---
+++ resources/js/utils/slider.js
@@ -0,0 +1,8 @@
+import Swiper, { Pagination, Navigation, Autoplay, Scrollbar, Thumbs, EffectFade } from "swiper";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+Swiper.use([Pagination, Navigation, Autoplay, Scrollbar, Thumbs, EffectFade])
+
